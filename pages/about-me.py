cc73338fc0057d3721025dc18a40888705485316
Victor Hugh <victorhughsilva@outlook.com>
2025-08-21 18:39:55 +0000

    2025

--- pages/about-me.py
+++ pages/about-me.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title='About Me', layout='wide')
 
 col1, col2, col3 = st.columns([1,2,1])
 with col2:
-    c1, c2 = st.columns([1,2])
+    c1, c2 = st.columns([1,1])
     with c1:
         st.image(
             'pages/etc/dev.jpg',
@@ -13,15 +13,14 @@ with col2:
     with c2:
         st.markdown('Victor Hugh')
         st.write('Estudante de Ciências e Tecnologia')
+    
+    c1, c2 = st.columns([1,1])
+    with c1:
+        st.link_button('🔗 LinkedIn','https://www.linkedin.com/in/victor-hugh-03031b23b')
+    with c2:
+        st.link_button('💻 GitHub','https://github.com/Hughboss432')
 
 st.markdown('---')
-st.markdown(
-    '''
-    🔗 [LinkedIn](https://www.linkedin.com/in/victor-hugh-03031b23b)
-    💻 [GitHub](https://github.com/Hughboss432)
-    ''',
-    unsafe_allow_html=True
-)
 # bottom buttons
 col1, col2, col3 = st.columns([1,2,1])
 with col1:

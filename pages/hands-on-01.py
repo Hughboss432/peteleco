58fa0eaafae8f06c0aa5a0fdaca6db0a6d5c107a
Victor Hugh <105021995+Hughboss432@users.noreply.github.com>
2025-08-27 05:27:30 +0000

    Delete pages/hands-on-01.py

--- pages/hands-on-01.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-with st.container(horizontal=True, horizontal_alignment='center'):
-    if st.button('< Voltar'):
-        st.switch_page('pages/about-me.py')
-    if st.button('Hands-on-02 >'):
-        st.switch_page('pages/hands-on-02.py')

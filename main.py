d050cb252f874592b1989f4ee091bec12a638e2f
Victor Hugh <105021995+Hughboss432@users.noreply.github.com>
2025-11-01 03:29:04 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ pg = st.navigation([
     st.Page('pages/home.py', title='Inicio'),
     st.Page('pages/about-me.py', title='Sobre mim'),
     st.Page('pages/proj-01.py', title='Projeto U1'),
+    st.Page('pages/proj-02.py', title='Projeto U2'),
 ])
 
 pg.run()

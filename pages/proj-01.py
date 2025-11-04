3282eac4990782416febe8d3d3f2842dcdeb1ecb
Victor Hugh <victorhughsilva@outlook.com>
2025-11-04 13:54:02 +0000

    U2

--- pages/proj-01.py
+++ pages/proj-01.py
@@ -8,9 +8,7 @@ from scipy.signal import find_peaks
 st.set_page_config(page_title="Projeto U1", layout="wide")
 
 st.title("Projeto U1")
-
-st.markdown('---')
-
+st.write("---")
 st.subheader('1 - Questão do projeto.')
 st.write('No arquivo deste experimento temos um arquivo .mat com:')
 st.write('* fs: frequência de amostragem (taxa de amostragem)')
@@ -19,24 +17,35 @@ st.write('* x: amostras dos sinal')
 st.write('O sinal x é composto por uma soma de 2 a 5 senos com frequências e fases diferentes. ' \
         'Seu objetivo é, por meio do sinal , estimar quantos senos compõem o sinal, quais suas ' \
         'frequências e respectivas fases.')
-
+st.write("---")
 st.subheader(
     '2 - A solução para nosso problema é utilizar uma Fast Fourier Transform (FFT) e analisar as fases com um Phase Spectrum.'
 )
 
-uploaded_file = st.file_uploader("Envie o arquivo .mat", type=["mat"])   # Upload do arquivo
-if uploaded_file is not None:
-    max_size_mb = 5                                                      # tamanho máximo permitido
-    if uploaded_file.size > max_size_mb * 1024 * 1024:
+if "use_test" not in st.session_state:                                        # Flag inicial
+    st.session_state.use_test = False
+
+user_uploaded_file = st.file_uploader("Envie seu arquivo .mat", type=["mat"]) # Upload do arquivo
+if user_uploaded_file is not None:                                            # Teste arquivo valido
+    st.session_state.use_test = False
+    max_size_mb = 5                                                           # tamanho máximo permitido
+    if user_uploaded_file.size > max_size_mb * 1024 * 1024:
         st.error(f"O arquivo é muito grande! Máximo permitido: {max_size_mb} MB")
-        uploaded_file = None                                             # ignora arquivo grande
+        user_uploaded_file = None                                             # ignora arquivo grande
+elif st.button("Ou utilize o meu arquivo do projeto, clique aqui!"):          # Determinar Teste
+    st.session_state.use_test = True
+
+if st.session_state.use_test:                                                 # Determinar se é teste ou não
+    uploaded_file = "pages/etc/sample.mat"
+else:
+    uploaded_file = user_uploaded_file
 
 if uploaded_file is not None:
     try:                                                                 
         mat_data = loadmat(uploaded_file)                                # Carregar o .mat
         keys = [k for k in mat_data.keys() if not k.startswith("__")]    # Ignora metadados e armazena variaveis
 
-        st.write("Variáveis encontradas no arquivo:")
+        st.write("* Variáveis encontradas no arquivo:")
         x = st.selectbox("Selecione o vetor amostras dos sinal", keys)
         t_sample = st.selectbox("Selecione o vetor com os instantes das amostras do sinal", keys)
         fs = st.selectbox("Selecione o vetor frequência de amostragem (taxa de amostragem)", keys)
@@ -73,7 +82,7 @@ if uploaded_file is not None:
                 "Graus": phase_peaks,
             })
             df_phase.index = range(1, len(df_fft) + 1)
-
+            st.divider()
             tab1, tab2, tab3 = st.tabs(["Sinal no tempo", "FFT", "Resultados"]) # Abas para gráficos
             with tab1:
                 fig_time = go.Figure()
@@ -102,18 +111,23 @@ if uploaded_file is not None:
                 st.plotly_chart(fig_phase, use_container_width=True)
 
                 st.write(
-                    "* Encontramos como resultado final usando 'np.angle' os graus das fases!", 
+                    "Encontramos como resultado final usando 'np.angle' os graus das fases!", 
                     df_phase
                 )
 
+                st.write("* Como um extra, podemos utilizar o python para ouvir nosso sinal! Aqui está ele em áudio:")
+                fs_a = fs/1000                                                                        # Formatando frequência
+                st.audio(signal, sample_rate=fs_a[0])                                                 # Sinal e frequência para o audio
+
+            st.write("---")
             st.subheader("3 - Códigos executados.")                                  # Mostrar código
             tab1, tab2, tab3 = st.tabs(["Tratando dados em .mat", "Plot de gráficos", "Resultados"])
             with tab1:
                 st.code(f"""
-mat_data = loadmat('{uploaded_file.name}')                       # Carregar o .mat
+mat_data = loadmat({{uploaded_file}})                            # Carregar o .mat
 keys = [k for k in mat_data.keys() if not k.startswith("__")]    # Ignora metadados e armazena variaveis
 
-st.write("Variáveis encontradas no arquivo:")                    # Mostrar e selecionar variaveis
+st.write("* Variáveis encontradas no arquivo:")                    # Mostrar e selecionar variaveis
 x = st.selectbox("Selecione o vetor amostras dos sinal", keys)
 t_sample = st.selectbox("Selecione o vetor com os instantes das amostras do sinal", keys)
 fs = st.selectbox("Selecione o vetor frequência de amostragem (taxa de amostragem)", keys)
@@ -122,7 +136,7 @@ t_sample = np.ravel(mat_data[t_sample])                          # Formata em ve
 fs = np.ravel(mat_data[fs])                                      # ---
 signal = np.ravel(mat_data[x])                                   # ---
 
-if st.button("Achar os resultados!"):
+if st.button("Analisar Sinal"):
     fft_vals = np.fft.rfft(signal)                               # * FFT do sinal (r para parte positiva)
     N = len(signal)                                              # N pontos
     T = 1/fs                                                     # passo de tempo T
@@ -187,9 +201,13 @@ if st.button("Analisar Sinal"):
         st.plotly_chart(fig_phase, use_container_width=True)
 
         st.write(
-            "* Encontramos como resultado final usando 'np.angle' os graus das fases!", 
+            "Encontramos como resultado final usando 'np.angle' os graus das fases!", 
             df_phase
         )
+                        
+        st.write("* Como um extra, podemos utilizar o python para ouvir nosso sinal! Aqui está ele em áudio:")
+        fs_a = fs/1000                                                                        # Formatando frequência
+        st.audio(signal, sample_rate=fs_a[0])                                                 # Sinal e frequência para o audio
 """, language="python")
 
     except Exception as e:

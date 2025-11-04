fa5f544193341f8f828fb2326da5ccc97fef5c63
Hughboss432 <victorhughsilva@outlook.com>
2025-11-04 14:02:21 -0300

    U2

--- pages/proj-02.py
+++ pages/proj-02.py
@@ -19,66 +19,118 @@ st.write('* (ii) submodulação;')
 st.write('* (iii) sobremodulação;')
 st.write('Explique a diferença entres eles e evidencie nos gráficos suas características.')
 
-tab1, tab2, tab3 = st.tabs(["Codigo exemplo","Resposta para o problema", "Plot de gráficos"])
+tab1, tab2, tab3 = st.tabs(["Código exemplo","Resposta para o problema", "Plot de gráficos"])
 with tab1:
     st.code(f"""
-    # Importa bibliotecas
-    import numpy as np
-    import matplotlib.pyplot as plt
-    from scipy import fftpack
+# Importa bibliotecas
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import fftpack
 
-    def mod_am_dsb(Ac,Am):
-    # Parâmetros do sinal
-    Ac = Ac                                                        # Amplitude da portadora
-    Am = Am                                                        # Amplitude do sinal
-    Mu = Am/Ac                                                     # Índice de modulação
+# Parâmetros do sinal
+Ac = 2                                                         # Amplitude da portadora
+Mu = 0.7                                                       # Índice de modulação
+fc = 25000                                                     # Frequência da portadora Hz
+fm = 2000
+N = 1000
+Ts = 1e-6                                                      # Tempo de amostragem pequeno (modelar sinal contínuo)
+t = np.arange(N)*Ts
+s = Ac*(1+Mu*np.cos(2*np.pi*fm*t))*np.cos(2*np.pi*fc*t)
 
-    fc = 25000                                                     # Frequência da portadora Hz
-    fm = 2000
-    N = 1000
-    Ts = 1e-6                                                      # Tempo de amostragem pequeno (modelar sinal contínuo)
-    t = np.arange(N)*Ts
-    s = Ac*(1+Mu*np.cos(2*np.pi*fm*t))*np.cos(2*np.pi*fc*t)
+# Cálculo da FFT de AM-DSB
+lfft = 30                                                      # Número pontos da fft
+k = np.arange(-lfft,lfft)                                      # Vetor de frequências
+S_f = 2.0*np.abs((fftpack.fft(s)))/N                           # Cálculo da FFT
+Ns = len(s)                                                    # Comprimento do sinal modulado
+Nk = len(k)                                                    # Comprimento do sinal em frequência
 
-    # Cálculo da FFT de AM-DSB
-    lfft = 30                                                      # Número pontos da fft
-    k = np.arange(-lfft,lfft)                                      # Vetor de frequências
-    S_f = 2.0*np.abs((fftpack.fft(s)))/N                           # Cálculo da FFT
-    Ns = len(s)                                                    # Comprimento do sinal modulado
-    Nk = len(k)                                                    # Comprimento do sinal em frequência
+# A fft em 30 pontos (para melhor visualização)
+S_f_new = np.zeros(Nk)                                         # Inicialização do vetor da frequência
+fsampling = 1/Ts                                               # Taxa de amostragem
+freq = (fsampling/Ns)*k                                        # Eixo de frequências
+for i in range(Nk):
+    kk = k[i]
+    if kk>=0:
+        S_f_new[i] = S_f[kk]
+    else :
+        S_f_new[i] = S_f[Ns+kk]
 
-    # A fft em 30 pontos (para melhor visualização)
-    S_f_new = np.zeros(Nk)                                         # Inicialização do vetor da frequência
-    fsampling = 1/Ts                                               # Taxa de amostragem
-    freq = (fsampling/Ns)*k                                        # Eixo de frequências
-    for i in range(Nk):
-        kk = k[i]
-        if kk>=0:
-            S_f_new[i] = S_f[kk]
-        else :
-            S_f_new[i] = S_f[Ns+kk]
+# Gráfico do AM-DSB no tempo
+plt.figure(1,[10,7])
+plt.subplot(211)
+plt.plot(t,s)
+plt.title("Sinal AM no tempo - AM-DSB (padrão)")
+plt.xlabel("Tempo [s]")
+plt.ylabel("Amplitude")
 
-    # Gráfico do AM-DSB no tempo
-    plt.figure(1,[10,7])
-    plt.subplot(211)
-    plt.plot(t,s)
-    plt.title("Sinal AM no tempo - AM-DSB (padrão)")
-    plt.xlabel("Tempo [s]")
-    plt.ylabel("Amplitude")
-
-    # Gráfico do AM-DSB na frequência
-    plt.subplot(212)
-    plt.title("AM-DSB na frequência")
-    plt.xlabel("Frequência [kHz]")
-    plt.ylabel("Magnitude")
-    plt.stem(freq/1e3,S_f_new)
-    plt.tight_layout()
-    plt.show()
+# Gráfico do AM-DSB na frequência
+plt.subplot(212)
+plt.title("AM-DSB na frequência")
+plt.xlabel("Frequência [kHz]")
+plt.ylabel("Magnitude")
+plt.stem(freq/1e3,S_f_new)
+plt.tight_layout()
+plt.show()
     """, language="python")
 
 with tab2:
-    st.write('adicionar depois')
+    st.write('Para os três casos, podemos obter os resultados alterando o valor da amplitude da portadora, já que não podemos alterar a amplitude do sinal modulador. Para cada caso, em ordem, temos:')
+    st.write('* ma = 1 (modulação de 100%), \n* ma < 1 (submodulação), \n* ma > 1 (sobremodulação).')
+    st.write('As alterações no código foram feitas para criar uma função que recebe o valor da amplitude da portadora, além de algumas melhorias nos gráficos de plotagem com o Plotly.')
+    st.code(f"""
+# Importa bibliotecas
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import fftpack
+import plotly.graph_objects as go
 
+def mod_am_dsb(Ac,Am):
+  # Parâmetros do sinal
+  Ac = Ac                                                        # Amplitude da portadora
+  Am = Am                                                        # Amplitude do sinal
+  Mu = Am/Ac                                                     # Índice de modulação
+
+  fc = 25000                                                     # Frequência da portadora Hz
+  fm = 2000
+  N = 1000
+  Ts = 1e-6                                                      # Tempo de amostragem pequeno (modelar sinal contínuo)
+  t = np.arange(N)*Ts
+  s = Ac*(1+Mu*np.cos(2*np.pi*fm*t))*np.cos(2*np.pi*fc*t)
+
+  # Cálculo da FFT de AM-DSB
+  lfft = 30                                                      # Número pontos da fft
+  k = np.arange(-lfft,lfft)                                      # Vetor de frequências
+  S_f = 2.0*np.abs((fftpack.fft(s)))/N                           # Cálculo da FFT
+  Ns = len(s)                                                    # Comprimento do sinal modulado
+  Nk = len(k)                                                    # Comprimento do sinal em frequência
+
+  # A fft em 30 pontos (para melhor visualização)
+  S_f_new = np.zeros(Nk)                                         # Inicialização do vetor da frequência
+  fsampling = 1/Ts                                               # Taxa de amostragem
+  freq = (fsampling/Ns)*k                                        # Eixo de frequências
+  for i in range(Nk):
+      kk = k[i]
+      if kk>=0:
+          S_f_new[i] = S_f[kk]
+      else :
+          S_f_new[i] = S_f[Ns+kk]
+
+  # Gráfico do AM-DSB no tempo usando Plotly
+  fig_time = go.Figure()
+  fig_time.add_trace(go.Scatter(x=t, y=s, mode='lines'))
+  fig_time.update_layout(title=f"Sinal AM no tempo - AM-DSB (padrão)",
+                         xaxis_title="Tempo [s]",
+                         yaxis_title="Amplitude")
+  fig_time.show()
+
+  # Gráfico do AM-DSB na frequência usando Plotly
+  fig_freq = go.Figure()
+  fig_freq.add_trace(go.Scatter(x=freq/1e3, y=S_f_new, mode='lines')) # Use lines instead of stem for plotly
+  fig_freq.update_layout(title=f"AM-DSB na frequência",
+                         xaxis_title="Frequência [kHz]",
+                         yaxis_title="Magnitude")
+  fig_freq.show()
+    """, language="python")
 with tab3:
     def mod_am_dsb(Ac,Am):                                             # Função resolução
         # Parâmetros do sinal
@@ -144,12 +196,12 @@ st.markdown('---')
 
 st.subheader('2 - Segunda questão do projeto.')
 st.write('Escreva um script em python que calcule o erro médio quadrático entre a envoltória ideal e a envoltória recuperada para os seguintes valores de τ: ')
-st.write('* (i) τ = 10^-1;')
-st.write('* (ii) τ = 2 * 10^-4;')
-st.write('* (iii) τ = 4 * 10^-3;')
+st.write('* (i) $τ = 1 * 10^{-1}$;')
+st.write('* (ii) $τ = 2 * 10^{-4}$;')
+st.write('* (iii) $τ = 4 * 10^{-3}$;')
 st.write('Disserte sobre os três valores de e o valor do erro. Quais os fatores causadores do erro?')
 
-tab1, tab2, tab3 = st.tabs(["Codigo exemplo","Resposta para o problema", "Plot de gráficos"])
+tab1, tab2, tab3 = st.tabs(["Código exemplo","Resposta para o problema", "Plot de gráficos"])
 with tab1:
     st.code(f"""
 import numpy as np
@@ -218,7 +270,77 @@ plt.show()
     """, language="python")
 
 with tab2:
-    st.write('adicionar depois')
+    st.write('As principais modificações no código foram: ')
+    st.write(
+        '\nCalcular o erro quadrático médio com a média de $ (x_{\t{envIdeal}} - x_{\t{env}})^2 $; ' \
+        'Alterar os gráficos para utilizar o Plotly.' \
+        'O principal ponto de ajustar o valor de τ está relacionado à descarga e à carga do capacitor. ' \
+        'Dependendo do valor escolhido, é possível recuperar com maior precisão a envoltória do sinal. No entanto, ' \
+        'valores elevados podem levar a erros, pois o capacitor pode descarregar-se muito rapidamente (resultando em perda de informações) ' \
+        'ou descarregar-se muito lentamente (resultando em retrabalho).')
+    st.write(
+        'Nesse caso, o valor de τ que mais se aproxima de recuperar os valores do nosso sinal é $ τ = 2 * 10^{-4} $, com o menor ' \
+        'erro quadrático médio de $ 5.37*10^{-2} $.'
+    )
+    st.code('''
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import fftpack
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+def deteccao_envelope(tau):
+  tau = tau                                                       # Constante de tempo do detector de envelope
+  Ts=1e-6                                                         # Definição do período
+  t = np.arange(1000)*Ts                                          # Definição do vetor tempo
+  fc = 10000                                                      # Frequência da portadora.
+  fm = 1500                                                       # Frequência do sinal
+  Mu = 0.6                                                        # Índice de modulaçao.
+  Ac = 1.0
+  x_AMo = Ac*(1.0+Mu*np.cos(2*np.pi*fm*t))*np.cos(2*np.pi*fc*t);  # Onda Modulada AM-DSB
+
+  x_envIdeal = np.abs(Ac*(1.0+Mu*np.cos(2*np.pi*fm*t)))           # Envoltória ideal
+
+  # Detector de envoltória
+  x_AM = x_AMo*(x_AMo>0)                                          # Efeito do diodo (semiciclo positivo)
+  x_env = np.zeros(len(x_AM))
+  Ns = len(x_AM)
+  out = -1
+  for i in range(Ns):
+      inp = x_AM[i]
+      if inp>=out:
+          out = inp            # Caso 1: x_am(t) > Vc(t) (carga do capacitor)
+      else:
+          out *= (1-Ts/tau)    # Caso 2: x_am(t) < Vc(t) (descarga do capacitor)
+      x_env[i] = out
+
+  # gráfico composto usando Plotly
+  fig = go.Figu ideal
+  fig_subplots.add_trace(go.Scatter(x=t*1000, y=x_envIdeal, mode='lines'), row=1, col=1)
+  fig_subplots.update_yaxes(title_text="Amplitude", row=1, col=1)
+  # Onda AM retificada
+  fig_subplots.add_trace(go.Scatter(x=t*1000, y=x_AM, mode='lines'), row=2, col=1)
+  fig_subplots.update_yaxes(title_text="Amplitude", row=2, col=1)
+  # Envoltoria recuperada
+  fig_subplots.add_trace(go.Scatter(x=t*1000, y=x_env, mode='lines'), row=3, col=1)
+  fig_subplots.update_yaxes(title_text="Amplitude", row=3, col=1)
+  fig_subplots.update_xaxes(title_text="Tempo (ms)", row=3, col=1)
+
+  fig_subplots.show()
+
+  return x_envIdeal, x_envre()
+  fig.add_trace(go.Scatter(x=t*1000, y=x_envIdeal, mode='lines', name='Envoltória ideal'))
+  fig.add_trace(go.Scatter(x=t*1000, y=x_AM, mode='lines', name='Onda AM retificada'))
+  fig.add_trace(go.Scatter(x=t*1000, y=x_env, mode='lines', name='Envoltória recuperada'))
+  fig.update_layout(title=f"Detecção de envoltória (τ = {tau:.2e})",
+                    xaxis_title="Tempo (ms)",
+                    yaxis_title="Amplitude")
+  fig.show()
+
+  # Definindo subplots
+  fig_subplots = make_subplots(rows=3, cols=1, subplot_titles=("Envoltória ideal", "Onda AM retificada", "Envoltória recuperada"))
+  # Envoltoria
+    ''', language='python')
 
 with tab3:
     def det_env(tau):
